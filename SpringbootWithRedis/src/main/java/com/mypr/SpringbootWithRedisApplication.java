688f3730b3f116a08156ba089e3944ab00add123
jitaranjan <jitaranjanbehera3@gmail.com>
2024-09-25 19:54:30 +0530

    added

---
+++ SpringbootWithRedis/src/main/java/com/mypr/SpringbootWithRedisApplication.java
@@ -0,0 +1,13 @@
+package com.mypr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWithRedisApplication{
+
+	public static void main(String[] args){
+		SpringApplication.run(SpringbootWithRedisApplication.class, args);
+	}
+
+}
---
+++ SpringbootWithRedis/src/test/java/com/mypr/SpringbootWithRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWithRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
