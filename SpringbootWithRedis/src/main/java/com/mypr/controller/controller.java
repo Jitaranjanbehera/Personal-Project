99f2eaa97ce9cc4759d77a3e9dd23b90781ebbd2
jitaranjan <jitaranjanbehera3@gmail.com>
2024-09-28 21:34:29 +0530

    change

--- SpringbootWithRedis/src/main/java/com/mypr/controller/controller.java
+++ SpringbootWithRedis/src/main/java/com/mypr/controller/controller.java
@@ -1,4 +1,23 @@
 package com.mypr.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class controller {
+
+    // Handle GET requests to "/api/greet"
+    @GetMapping("/greet")
+    public String greet() {
+        return "Hello, World!"+1;
+    }
+
+    // Handle GET requests to "/api/greet/{name}" where {name} is a path variable
+    @GetMapping("/greet/{name}")
+    public String greetWithName(@PathVariable String name){
+        return "Hello, " + name + "Love You";
+    }
 }
